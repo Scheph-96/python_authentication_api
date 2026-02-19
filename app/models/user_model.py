45fef3d4adf311393c7380fe6a3d5febd4531056
scheph <omarscheph18@gmail.com>
2026-02-19 00:42:23 +0000

    effective_permissions field added to user mode && role management files created

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -9,12 +9,13 @@ from bson import ObjectId
 passwordHash = PasswordHasher()
 
 class User:
-    def __init__(self,username: str, email: str, auth_version: int = 0, _id: ObjectId = None, hashed_password: str | None = None, is_verified:bool = False, created_at: datetime | None = None):
+    def __init__(self,username: str, email: str, auth_version: int = 0, _id: ObjectId = None, hashed_password: str | None = None, effective_permissions: list = [], is_verified:bool = False, created_at: datetime | None = None):
         self.username = username
         self.email = email
         self.auth_version = auth_version
         self._id = _id
         self.hashed_password = hashed_password
+        self.effective_permissions: effective_permissions
         self.is_verified = is_verified
         self.created_at = created_at or datetime.now(timezone.utc)
         
@@ -26,6 +27,12 @@ class User:
     def verify_password(self, raw_password: str) -> bool:
         return passwordHash.verify(self.hashed_password, raw_password)
     
+    def add_effective_permissions(self, permission: str) -> None:
+        self.effective_permissions.append(permission)
+        
+    def remove_effective_permissions(self, permission: str) -> None:
+        self.effective_permissions.remove(permission)
+    
     # -------- Serialization --------
     
     def to_dict(self) -> dict:
@@ -34,6 +41,7 @@ class User:
             "email": self.email,
             "auth_version": self.auth_version,
             "hashed_password": self.hashed_password,
+            "effective_permissions": self.effective_permissions,
             "is_verified": self.is_verified,
             "created_at": self.created_at
         }
